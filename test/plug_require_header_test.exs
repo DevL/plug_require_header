25883d88bd9002754dd63265dc3e9824d8ceb4cd
Lennart Fridén <lennart@devl.se>
2015-04-03 13:59:04 +0200

    Flesh out test cases.

--- test/plug_require_header_test.exs
+++ test/plug_require_header_test.exs
@@ -12,10 +12,29 @@ defmodule PlugRequireHeaderTest do
     assert response.status == Status.code(:forbidden)
   end
 
+  test "block request with a header set, but without the required header" do
+    connection = conn(:get, "/") |> put_req_header("x-wrong-header", "whatever")
+    response = TestApp.call(connection, @options)
+
+    assert response.status == Status.code(:forbidden)
+  end
+
   test "extract the required header and assign it to the connection" do
     api_key = "12345"
 
-    connection = conn(:get, "/") |> put_req_header "x-api-key", api_key
+    connection = conn(:get, "/") |> put_req_header("x-api-key", api_key)
+    response = TestApp.call(connection, @options)
+
+    assert response.status == Status.code(:ok)
+    assert response.resp_body == api_key
+  end
+
+  test "extract the required header even if multiple headers are set" do
+    api_key = "12345"
+
+    connection = conn(:get, "/")
+    |> put_req_header("x-api-key", api_key)
+    |> put_req_header("x-wrong-header", "whatever")
     response = TestApp.call(connection, @options)
 
     assert response.status == Status.code(:ok)
